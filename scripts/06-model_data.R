fe882194bba4da78c29e203fccf2472a2644199f
Colin Sihan Yang <colinsihan.yang@mail.utoronto.ca>
2024-11-25 23:05:48 -0500

    update the paper.qmd, model part and train test validation part

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -11,27 +11,60 @@
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(here)
+library(arrow)
 
+set.seed(123)
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+cleaned_lol_data <- read_parquet(here("data/02-analysis_data/cleaned_lol_data.parquet"))
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+set.seed(123)
+formula = KDA ~ visionscore + totalgold + total.cs + damagetochampions + (1 | side) + + (1 | gamelength) + (1 | position)
+priors = normal(0, 2.5, autoscale = TRUE)
+
+bayesian_model_1 <- stan_glmer(
+  formula = formula,
+  data = cleaned_lol_data,
+  family = gaussian(),
+  prior = priors,
+  prior_intercept = priors,
+  seed = 123,
+  cores = 4,
+  adapt_delta = 0.95
+)
+
+#### Save model ####
+saveRDS(
+  bayesian_model_1,
+  file = "models/bayesian_model_1.rds"
+)
+
+train_indices = sample(seq_len(nrow(cleaned_lol_data)), size = 0.7 * nrow(cleaned_lol_data))
+
+train_data = cleaned_lol_data[train_indices, ]
+test_data = cleaned_lol_data[-train_indices, ]
+write_parquet(train_data, "data/02-analysis_data/train_data.parquet")
+write_parquet(test_data, "data/02-analysis_data/test_data.parquet")
+
+bayesian_model_train <- stan_glmer(
+  formula = formula,
+  data = train_data,
+  family = gaussian(),
+  prior = priors,
+  prior_intercept = priors,
+  seed = 123,
+  cores = 4,
+  adapt_delta = 0.95
+)
 
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  bayesian_model_1,
+  file = "models/bayesian_model_train.rds"
 )
+
+
 
 
